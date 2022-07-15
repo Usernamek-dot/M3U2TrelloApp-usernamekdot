8a3135c520da6bab1183cec597d60dd74e5bb765
Usernamek-dot <66336947+Usernamek-dot@users.noreply.github.com>
2022-07-15 11:24:51 -0500

    Static fron-end v.1  -> cambio logo

--- my-app/src/components/AppBar/index.js
+++ my-app/src/components/AppBar/index.js
@@ -4,7 +4,7 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";import CssBaseline from "@mui/material/CssBaseline";
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
 import AlertDialog from "../AddForm";
 
 
@@ -23,7 +23,8 @@ export default function ButtonAppBar() {
               aria-label="menu"
               sx={{ mr: 2 }}
             >
-              <MenuIcon />
+              <AccountTreeIcon
+Static fron-end v.1  />
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Trello Demo
