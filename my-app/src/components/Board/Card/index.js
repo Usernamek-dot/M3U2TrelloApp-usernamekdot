2b09dcf3ce5dd155d7c3acb415861bcbd5d20af1
Karol <kypacheco07@misena.edu.co>
2022-07-18 23:02:17 -0500

    Fixed Icons import issues

--- my-app/src/components/Board/Card/index.js
+++ my-app/src/components/Board/Card/index.js
@@ -1,6 +1,8 @@
 import { Grid, Card, CardHeader, CardContent, Divider } from "@mui/material";
 import DraggableComponent from "../Draggable";
-import { PendingActionsOutlinedIcon, PendingOutlinedIcon, CheckCircleOutlineOutlinedIcon } from '@mui/icons-material';
+import PendingActionsOutlinedIcon from '@mui/icons-material/PendingActionsOutlined';
+import PendingOutlinedIcon from '@mui/icons-material/PendingOutlined';
+import CheckCircleOutlineOutlinedIcon from '@mui/icons-material/CheckCircleOutlineOutlined';
 
 export default function CardComponent() {
   return (
